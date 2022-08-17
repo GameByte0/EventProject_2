a6ca619795e05397a99f84ab6ce99dfde5f39863
GameByte0 <47858521+GameByte0@users.noreply.github.com>
2022-08-17 17:58:52 +0400

    Object pooling added

--- Project_Tost/Assets/App/Scripts/SpawnerController.cs
+++ Project_Tost/Assets/App/Scripts/SpawnerController.cs
@@ -1,31 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class SpawnerController : MonoBehaviour
 {
+	[SerializeField] private List<GameObject> pooledObjects;
+
 	[SerializeField] private GameObject[] tostComponents;
 
-	[SerializeField] private GameObject mainCamera;
-
-	[SerializeField] private bool componentState;
+	[SerializeField] private int poolAmount;
 
 
+	GameObject tostComponentInstance;
 
-	private int currentComponent;
+	#region UnityEditor
+	private void Start()
+	{
+		pooledObjects = new List<GameObject>();
+		GameObject tostComponentInstance;
+
+		foreach (GameObject component in tostComponents)
+		{
+			for (int i = 0; i < poolAmount; i++)
+			{
+				tostComponentInstance = Instantiate(component,transform);
+				tostComponentInstance.transform.SetParent(transform);
+				tostComponentInstance.SetActive(false);
+				pooledObjects.Add(tostComponentInstance);
+			}
+		}
+
+	}
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			currentComponent = Random.Range(0, tostComponents.Length);
-
-			GameObject tostComponentInstance = Instantiate(tostComponents[currentComponent], transform.position, transform.rotation);
-
-			tostComponentInstance.GetComponent<Rigidbody>().isKinematic = false;
-
-			mainCamera.transform.position += new Vector3(0f, tostComponentInstance.transform.localScale.y, 0f);
-
-			transform.position = new Vector3(0f, mainCamera.transform.position.y, 0f);
+			pooledObjects.Remove(tostComponentInstance);
+			tostComponentInstance.SetActive(true);
 		}
 	}
+	#endregion
+
 }
